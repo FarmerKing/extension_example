42fb2232f505c598f20c1a381802c039222f95f2
FarmerKing <king_chen@mail2000.com.tw>
2014-01-14 14:24:28 +0800

    update chrome_version2 skeleton

--- chrome_version2/script.js
+++ chrome_version2/script.js
@@ -1,30 +1,40 @@
-var process = function(element){
+var mutationHandler = function(mutations){
+    var isSkipChildList = false;
+    mutations.forEach(function(mutation) {
+        if(mutation.type !== "childList"){
+            if( mutation.target.classList.contains("UFILikeLink") && 
+                mutation.attributeName === "data-ft"){
+                process(mutation.target.parentElement,true);
+            }
+        }else if(isSkipChildList){
+        }else if( mutation.target.tagName.toLowerCase() === "div" && 
+                  (mutation.target.classList.contains("_5pcb") || 
+                   mutation.target.classList.contains("_4ikz")) ){
+            process();
+            isSkipChildList=true;
+        }
+    });
+};
+var mutationObserver  = new MutationObserver(mutationHandler);
+
+var process = function(element,isForce){
+    element = typeof element !== 'undefined' ? element : document;
+    isForce = typeof isForce !== 'undefined' ? isForce : false;
+
     var nodeList = element.querySelectorAll("a.UFILikeLink");
     for (var i = 0; i < nodeList.length; ++i) {
         var node = nodeList[i]; 
+
+        if( !isForce && node.classList.contains("replaced")) continue;
         if( node.getAttribute("data-ft") === '{"tn":">"}' ){
             node.innerHTML = chrome.i18n.getMessage("like_button"); //"I know!"; 
         }else{
             node.innerHTML = chrome.i18n.getMessage("already_like"); //"aleady viewed!"; 
         }
+
+        node.classList.add("replaced");
     }
 };
 
-process(document.body);
-
-new MutationObserver(function(mutations){
-    mutations.forEach(function(mutation) {
-        switch( mutation.type){
-        case "attributes": 
-            if( typeof mutation.target.className !== "undefined" && 
-                mutation.target.className === "UFILikeLink" && 
-                mutation.attributeName === "data-ft"){
-                process(mutation.target.parentElement);
-            }
-            break; 
-        case "childList": 
-            process( mutation.target );
-            break;
-        }
-    });
-}).observe(document.body, { attributeOldValue:true, childList: true, subtree: true, attributes:true });
+process();
+mutationObserver.observe(document.body, { childList: true, subtree: true, attributes:true });
